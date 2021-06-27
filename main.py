52c51670aa35a0d3bbb97cdfe833274aaa1ce174
Simon Phillips <>
2021-06-27 22:56:57 +0000

    Display only relevant  columns (still only every 7th line)

--- main.py
+++ main.py
@@ -2,10 +2,19 @@ def openfile():
   f = open('countries.csv')
   return f
 
-def displayinfo():
+def displayinfo_1():
   global infofile
   lines = infofile.readlines()
   print(lines)
+
+def displayinfo():
+  global infofile
+  lines = infofile.readlines()
+  for i in range(0,len(lines),7):
+    list_line = lines[i].split(',')
+    """Country,Region,Population,Area (sq. mi.),Pop. Density (per sq. mi.),Phones (per 1000)"""
+    print(list_line[0],list_line[1].strip(),list_line[2] ,list_line[3],list_line[10]),
+    #print(i,': ',lines[i])
 
 infofile = openfile()
 displayinfo()
