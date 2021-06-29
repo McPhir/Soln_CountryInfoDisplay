f8e5c5c276c217a04395234efabe16632f719a07
Simon Phillips <75336829+McPhir@users.noreply.github.com>
2021-06-29 00:21:20 +0000

    display info in aligned columns

--- main.py
+++ main.py
@@ -10,12 +10,24 @@ def displayinfo_1():
 def displayinfo():
   global infofile
   lines = infofile.readlines()
-  for i in range(0,len(lines),7):
+  #print heading first as they are strings
+  list_line = lines[0].split(',')
+  """Country,Region,Population,Area (sq. mi.),Pop. Density (per sq. mi.),Phones (per 1000)"""
+  print(list_line[0].strip().ljust(20,' '),
+  list_line[1].strip().ljust(20,' '),
+  list_line[2].strip().ljust(15,' '),
+  str(list_line[3]).strip().ljust(15,' '),
+  str(list_line[10]).strip().ljust(10,' ') )
+
+  for i in range(1,len(lines),7):
     list_line = lines[i].split(',')
     """Country,Region,Population,Area (sq. mi.),Pop. Density (per sq. mi.),Phones (per 1000)"""
-    print(list_line[0],list_line[1].strip(),list_line[2] ,list_line[3],list_line[10]),
-    #print(i,': ',lines[i])
+    print(list_line[0].strip().ljust(20,' '),
+    list_line[1].strip().ljust(20,' '),
+    "{:,}".format(int(list_line[2])).strip().ljust(15),
+    "{:,}".format(int(list_line[3])).strip().ljust(15),
+    list_line[10].strip().ljust(10) )
+    #print(i,': ',lines[i],)
 
 infofile = openfile()
 displayinfo()
-
