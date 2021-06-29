3303ec23336056b46493bc6d96e750fa1374d435
Simon Phillips <75336829+McPhir@users.noreply.github.com>
2021-06-29 03:42:52 +0000

    Added the user prompt for the region (string region)

--- main.py
+++ main.py
@@ -7,9 +7,27 @@ def displayinfo_1():
   lines = infofile.readlines()
   print(lines)
 
-def displayinfo():
+def getregions():
+  """this reads the csv file and generates a list of the regions - with no duplicates"""
+  regions = []
+  #lines = infofile.readlines()
+  for line in lines:
+    list_lines = line.split(',')
+    region = list_lines[1]
+    if region in regions:
+      #do nothing
+      pass
+    else:
+      regions.append(region)
+  for r in regions:
+    print(r)
+  return regions
+
+
+
+def displayinfo_2():
   global infofile
-  lines = infofile.readlines()
+  #lines = infofile.readlines()
   #print heading first as they are strings
   list_line = lines[0].split(',')
   """Country,Region,Population,Area (sq. mi.),Pop. Density (per sq. mi.),Phones (per 1000)"""
@@ -29,5 +47,35 @@ def displayinfo():
     list_line[10].strip().ljust(10) )
     #print(i,': ',lines[i],)
 
+def displayinfo(region):
+  global infofile
+  #lines = infofile.readlines()
+  #print heading first as they are strings
+  list_line = lines[0].split(',')
+  """Country,Region,Population,Area (sq. mi.),Pop. Density (per sq. mi.),Phones (per 1000)"""
+  print(list_line[0].strip().ljust(20,' '),
+  list_line[1].strip().ljust(20,' '),
+  list_line[2].strip().ljust(15,' '),
+  str(list_line[3]).strip().ljust(15,' '),
+  str(list_line[10]).strip().ljust(10,' ') )
+
+  for i in range(1,len(lines)):
+    list_line = lines[i].split(',')
+    """Country,Region,Population,Area (sq. mi.),Pop. Density (per sq. mi.),Phones (per 1000)"""
+    if region.strip().lower() == list_line[1].strip().lower() :
+      print(list_line[0].strip().ljust(20,' '),
+      list_line[1].strip().ljust(20,' '),
+      "{:,}".format(int(list_line[2])).strip().ljust(15),
+      "{:,}".format(int(list_line[3])).strip().ljust(15),
+      list_line[10].strip().ljust(10) )
+    #print(i,': ',lines[i],)
+
 infofile = openfile()
-displayinfo()
+lines = infofile.readlines()
+getregions()
+#displayinfo()
+
+user_region = input("Enter the region\n")
+while user_region != "quit" :
+  displayinfo(user_region)
+  user_region = input("Enter the region\n")
